0ca4563b5b30f5c09e5705d50c2055abd7a385c3
Shen Wu <Hsi-hsuan.Wu@mediatek.com>
2022-10-08 18:45:39 +0800

    [ALPS07635676] camsys: fix default single sv hw num for mt6886
    [Detail]
    fix default single sv hw num for mt6886
    
    MTK-Commit-Id: 04658521ffdcbe9cc1f629020a7df693bca65c18
    
    CR-Id: ALPS07635676
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Shen Wu <Hsi-hsuan.Wu@mediatek.com>
    Change-Id: Ie3d04db8f791162d2da128de7e2c4c720b699773

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-plat-mt6886.c
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-plat-mt6886.c
@@ -383,7 +383,7 @@ static const struct plat_v4l2_data mt6886_v4l2_data = {
 
 	.get_mraw_stats_cfg_param = get_mraw_stats_cfg_param,
 
-	.reserved_camsv_dev_id = 2,
+	.reserved_camsv_dev_id = 3,
 };
 
 struct camsys_platform_data mt6886_data = {
