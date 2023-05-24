cfde86bd04f6ad265b4124eeb1960d7793b3e0c5
Duo Yu <duo.yu@mediatek.com>
2023-05-24 11:52:01 +0800

    [ALPS08022581] display_pq: fix KE issue
    add lock to prevent race condition
    
    MTK-Commit-Id: e917e0a99b84c0d7f5f62aac9d8be9812b84cedc
    
    Signed-off-by: Duo Yu <duo.yu@mediatek.com>
    CR-Id: ALPS08022581
    Feature: display_pq
    Change-Id: Icbafd594ff8fb76ef7eb8f25679778c8e0b9cbe1

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_tdshp.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_tdshp.c
@@ -423,12 +423,19 @@ int mtk_drm_ioctl_tdshp_get_size(struct drm_device *dev, void *data,
 	u32 width = 0, height = 0;
 	struct DISP_TDSHP_DISPLAY_SIZE *dst =
 			(struct DISP_TDSHP_DISPLAY_SIZE *)data;
+	struct mtk_drm_private *private = dev->dev_private;
 
 	pr_notice("%s", __func__);
 
 	crtc = list_first_entry(&(dev)->mode_config.crtc_list,
 		typeof(*crtc), head);
 
+	if (IS_ERR_OR_NULL(private)) {
+		DDPMSG("%s, invalid private\n", __func__);
+		return -EINVAL;
+	}
+
+	mutex_lock(&private->commit.lock);
 	mtk_drm_crtc_get_panel_original_size(crtc, &width, &height);
 	if (width == 0 || height == 0) {
 		DDPFUNC("panel original size error(%dx%d).\n", width, height);
@@ -440,6 +447,7 @@ int mtk_drm_ioctl_tdshp_get_size(struct drm_device *dev, void *data,
 	g_tdshp_size.lcm_height = height;
 
 	disp_tdshp_wait_size(60);
+	mutex_unlock(&private->commit.lock);
 
 	pr_notice("%s ---", __func__);
 	memcpy(dst, &g_tdshp_size, sizeof(g_tdshp_size));
