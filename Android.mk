0cb59198faa35d9caf1e957e87b239bdc008d1fb
anderson.chen <anderson.chen@nothing.tech>
2024-03-01 14:02:32 +0800

    Use correct dts by ro.boot.dtbo_idx
    Change-Id: I4f805985f3ab1b0ac003c0ee544aee81506d155a

--- Android.mk
+++ Android.mk
@@ -115,6 +115,7 @@ MTK_DTBIMAGE_DTS := $(addsuffix .dts,$(addprefix $(KERNEL_DIR)/arch/$(KERNEL_TAR
 include device/mediatek/build/core/build_dtbimage.mk
 
 MTK_DTBOIMAGE_DTS := $(addsuffix .dts,$(addprefix $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/,$(PROJECT_DTB_NAMES)))
+MTK_DTBOIMAGE_DTS += $(wildcard $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/$(PROJECT_DTB_NAMES)_*.dts)
 include device/mediatek/build/core/build_dtboimage.mk
 
 endif #LINUX_KERNEL_VERSION
