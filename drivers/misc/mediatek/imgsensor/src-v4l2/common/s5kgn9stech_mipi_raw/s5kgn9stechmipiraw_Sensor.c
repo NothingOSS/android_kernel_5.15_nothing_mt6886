ca74bf4f9bd53f65125a5f1183c703e20dafd7b9
huang.chen <huangchen0715@thundersoft.com>
2024-03-07 17:10:28 +0800

    Modify backcamera ldo.
    Change-Id: I8d1399db096a4c3c86a7020f6eddc31b85b209f7

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/s5kgn9stech_mipi_raw/s5kgn9stechmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/s5kgn9stech_mipi_raw/s5kgn9stechmipiraw_Sensor.c
@@ -10936,7 +10936,7 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_RST, 0, 5},
 	{HW_ID_DOVDD, 1800000, 1},
-	//{HW_ID_AVDD1, 2800000, 10},
+	{HW_ID_AVDD, 2800000, 5},
 	//{HW_ID_PDN, 1, 3},
 	//{HW_ID_DVDD1, 1050000, 10},
 	//{HW_ID_AFVDD, 2800000,  5},
