9327c1e926810e6d98c44d38f6e68e96132db4a8
Kenny Liu <kenny.liu@mediatek.com>
2024-05-21 11:39:18 +0800

    [ALPS08912304] mminfra: wait 3 ms if scmi timeout and add time stamp
    [Description]
    Wait 3 ms if scmi timeout and add time stamp
    
    [Test]
    tc19/20
    
    MTK-Commit-Id: a21e05ba4b5501917d79b1fc0a258aedb3e7b60d
    
    Signed-off-by: Kenny Liu <kenny.liu@mediatek.com>
    CR-Id: ALPS08912304
    Feature: mminfra
    Change-Id: Ia2d1e38b65d6ba6a0d5eaa1e86d1b57f8c6df879

--- drivers/misc/mediatek/mminfra/mtk-mminfra-debug.c
+++ drivers/misc/mediatek/mminfra/mtk-mminfra-debug.c
@@ -5,6 +5,7 @@
  */
 
 #include <linux/clk.h>
+#include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of_platform.h>
@@ -95,16 +96,26 @@ static bool mminfra_check_scmi_status(void)
 static void do_mminfra_bkrs(bool is_restore)
 {
 	int err;
+	u64 start_ts, start_osts, end_ts, end_osts;
 
 	if (mminfra_check_scmi_status()) {
-		u64 bkrs_ts = sched_clock();
-		u64 bkrs_osts = __arch_counter_get_cntvct();
+		start_ts = sched_clock();
+		start_osts = __arch_counter_get_cntvct();
 
 		err = scmi_tinysys_common_set(tinfo->ph, feature_id,
 				2, (is_restore)?0:1, 0, 0, 0);
-		if (err)
-			pr_notice("%s: call scmi_tinysys_common_set(%d) err=%d osts:%llu ts:%llu\n",
-				__func__, is_restore, err, bkrs_osts, bkrs_ts);
+
+		if (err) {
+			end_ts = sched_clock();
+			end_osts = __arch_counter_get_cntvct();
+			pr_notice("%s: call scmi(%d) err=%d osts:%llu ts:%llu\n",
+				__func__, is_restore, err, start_osts, start_ts);
+			if (err == -ETIMEDOUT) {
+				pr_notice("%s: call scmi(%d) timeout osts:%llu ts:%llu\n",
+					__func__, is_restore, end_osts, end_ts);
+				mdelay(3);
+			}
+		}
 	}
 }
 
