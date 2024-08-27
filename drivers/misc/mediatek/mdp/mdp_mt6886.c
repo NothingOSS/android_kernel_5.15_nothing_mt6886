bad7c10adfa96ee8e6889326b672160c640091c6
ot_tushar.gupta <ot_tushar.gupta@mediatek.com>
2024-08-27 18:52:24 +0800

    [ALPS09031997] mdp: svp hdr engine mapping
    [Description]
    Map mdp engine to secure region Svp case
    
    [Test]
    Boot pass
    
    MTK-Commit-Id: d63d1282e031c06a1852cd406123768f93bb20df
    
    Change-Id: I208744f4e0c5e7576358bde4c9855b61b47baaf3
    CR-Id: ALPS09031997
    Feature: mdp driver
    Signed-off-by: ot_tushar.gupta <ot_tushar.gupta@mediatek.com>

--- drivers/misc/mediatek/mdp/mdp_mt6886.c
+++ drivers/misc/mediatek/mdp/mdp_mt6886.c
@@ -1594,6 +1594,10 @@ static void mdp_readback_aal_by_engine(struct cmdqRecStruct *handle,
 		CMDQ_ERR("%s not support\n", __func__);
 		return;
 	}
+#ifdef CMDQ_SECURE_PATH_SUPPORT
+	if (handle->secData.is_secure)
+		engine = engine - CMDQ_ENG_MDP_AAL0 + CMDQ_SEC_MDP_AAL0;
+#endif
 
 	cmdq_mdp_get_func()->mdpReadbackAal(handle, engine, base, pa, param, pipe);
 }
@@ -1617,6 +1621,10 @@ static void mdp_readback_hdr_by_engine(struct cmdqRecStruct *handle,
 		CMDQ_ERR("%s not support\n", __func__);
 		return;
 	}
+#ifdef CMDQ_SECURE_PATH_SUPPORT
+	if (handle->secData.is_secure)
+		engine = engine - CMDQ_ENG_MDP_HDR0 + CMDQ_SEC_MDP_HDR0;
+#endif
 
 	cmdq_mdp_get_func()->mdpReadbackHdr(handle, engine, base, pa, param, pipe);
 }
