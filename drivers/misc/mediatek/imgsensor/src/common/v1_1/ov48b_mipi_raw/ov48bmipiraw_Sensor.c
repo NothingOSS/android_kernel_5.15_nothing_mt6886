e34b0c535fe67c029124b92fd9391c6a4151b048
ot908181 <ot_junjie.tang@mediatek.com>
2024-10-29 18:37:30 +0800

    [ALPS09431943] Cam:Fix its For sensorFusion fail
    [Description]
    Fix mt6835 its For sensorFusion fail
    
    [Test]
    tc29 tc30
    
    MTK-Commit-Id: d20333cdf10858c492eef37a44b86847a0ea052e
    
    CR-Id: ALPS09431943
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Icbc07a371d734c78e0a3b121cc48c4d92b57b32e
    Signed-off-by: ot908181 <ot_junjie.tang@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/ov48b_mipi_raw/ov48bmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/ov48b_mipi_raw/ov48bmipiraw_Sensor.c
@@ -2074,7 +2074,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 	case SENSOR_FEATURE_GET_OFFSET_TO_START_OF_EXPOSURE:
 		if (IS_MT6835(g_platform_id))
 			*(MUINT32 *)(uintptr_t)(*(feature_data + 1))
-				= 1700000;
+				= 2939600;
 		break;
 	case SENSOR_FEATURE_GET_PERIOD_BY_SCENARIO:
 		switch (*feature_data) {
