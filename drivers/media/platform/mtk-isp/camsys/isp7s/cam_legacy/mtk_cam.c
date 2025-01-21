cb61d1ed1f0a1d06d88fae5aff3e6d6b814307b7
Johnny Jhu <jyun-hua.jhu@mediatek.com>
2025-01-21 15:00:39 +0800

    [ALPS09676587] camsys: isp7s: remove subdev register out of lock
    [Description]
    v4l2_dev in k-mainline add videodev_lock in video_device_register,
    it will cause camsys hold graph_mutex -> videodev_lock -> graph_mutex,
    which also cause the tool detects deadlock risk in this repeated order.
    Therefore, we remove this out of graph_mutex holding.
    
    [Test]
    MTBF
    
    MTK-Commit-Id: 88016ad2b7aea41ee80c6d78f6288b470f30f220
    
    CR-Id: ALPS09676587
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Johnny Jhu <jyun-hua.jhu@mediatek.com>
    Change-Id: I91d834cba91f9b7da34bd6fb5810202485dd13a1

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam.c
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam.c
@@ -9946,9 +9946,9 @@ static int mtk_cam_master_bind(struct device *dev)
 
 	mutex_lock(&cam_dev->v4l2_dev.mdev->graph_mutex);
 	mtk_cam_create_links(cam_dev);
+	mutex_unlock(&cam_dev->v4l2_dev.mdev->graph_mutex);
 	/* Expose all subdev's nodes */
 	ret = v4l2_device_register_subdev_nodes(&cam_dev->v4l2_dev);
-	mutex_unlock(&cam_dev->v4l2_dev.mdev->graph_mutex);
 	if (ret) {
 		dev_dbg(dev, "Failed to register subdev nodes\n");
 		goto fail_unreg_mraw_entities;
